8e466cc9d17b7116656290acccdf6d3dc918b86b
tommenbro <74247764+tommenbro@users.noreply.github.com>
2022-06-20 01:28:10 +0200

    image carousel component for detail page works

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,9 @@
 import { useParams } from "react-router-dom";
 import { Component } from "react";
+import Carousel from "./components/Carousel";
 
 class Details extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { loading: true };
-  }
+  state = { loading: true };
 
   async componentDidMount() {
     const res = await fetch(
@@ -22,10 +19,12 @@ class Details extends Component {
       return <h2>Loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
